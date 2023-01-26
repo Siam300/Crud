2d8ae184df6888e166977a4a8e146b0c83427a5b
Siam300 <siamkhan8523@gmail.com>
2023-01-26 19:13:37 +0600

    updated

--- crud/crud/views.py
+++ crud/crud/views.py
@@ -1,4 +1,4 @@
-
+from django.shortcuts import render
 from django.shortcuts import redirect, render
 from news.models import DailyNews
 
@@ -11,13 +11,14 @@ def INDEX(request):
 
 def ADD(request):
     if request.method ==  "POST":
-        newsTitle = request.POST.get('news_title')
-        details = request.POST.get('dtls')
+        NewsTitle = request.POST.get('news_title')
+        Details = request.POST.get('dtls')
 
         nws = DailyNews(
-            newsTtitle = newsTitle,
-            details = details
+             newsTitle = NewsTitle,
+             details = Details,
         )
         nws.save()
         return redirect('home')
     return render(request, 'index.html')
+
